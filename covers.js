b2696cb776652d961def3bb32ba0d5e0873d3bc1
William Dyce <wilbefast@gmail.com>
2015-05-17 19:10:22 +0200

    full cover set, filter by technology

--- covers.js
+++ covers.js
@@ -3,12 +3,27 @@ var covers = (function() {
 
 	var _init = function(settings)
 	{
+		var $tech = $("#technology");
 
 		$.getJSON( "covers.json", function( data ) {
 		  $.each( data, function( key, val ) {
-		  	console.log(key, val);
+		  	var c = "mix"
+		  	for(var i in val.tech)
+		  	{
+		  		var t = val.tech[i].toLowerCase().replace(/ |\./g, "_");
+		  		c += " tech_" + t;
+		  		if(!(t in $tech))
+		  		{
+		  			$tech[t] = $("<button/>", {
+		  				"text" : val.tech[i],
+		  				"class" : "filter",
+		  				"data-filter" : ".tech_" + t
+		  			});
+					  $tech[t].insertAfter($tech);
+		  		}
+		  	}
 			  var cover = $("<div/>", { 
-			  	"class": "mix category-1" 
+			  	"class": c
 			  });
 			  cover.css("background-image", "url(covers/" + val.image + ")");  
 			  cover.appendTo(settings.cover_container);
