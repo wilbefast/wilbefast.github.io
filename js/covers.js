8c9f3be012bcdced2a4960ec0c48e102a53612a6
William Dyce <wilbefast@gmail.com>
2015-05-18 01:22:24 +0200

    image and title in popups

--- js/covers.js
+++ js/covers.js
@@ -1,11 +1,23 @@
 var covers = (function() {
 
+	var _popup;
+
+	var _covers = {}
+
+	var _select = function(cover) {
+		_popup.modal('show');
+		_popup.find("#popup_image").attr("src", "covers/" + cover.image);
+		_popup.find("#popup_title").text(cover.name);
+		$.getJSON("info/" + cover.key + ".json", function( data ) {
+		});
+	}
 
 	var _init = function(settings)
 	{
 		var sort = settings.sorting;
 		var tech = settings.technology;
 		var container = settings.cover_container;
+		_popup = settings.popup;
 
 		$.getJSON( "covers.json", function( data ) {
 		  $.each( data, function( key, val ) {
@@ -23,26 +35,34 @@ var covers = (function() {
 					  option.appendTo(tech);
 		  		}
 		  	}
-		  	var link = $("<a/>", {
-		  		"data-toggle" : "modal",
-		  		"data-target" : "#popup"
-		  	});
 			  var cover = $("<div/>", { 
 			  	"class": c,
 			  	"data-year" : val.started,
 			  	"data-name" : val.name.charCodeAt(0),
+			  	"data-awesome" : val.awesome
 			  });
+			  val.image = (val.image || key + ".png");
 			  var image = $("<div/>", {
 			  	"style" : "background-image : url('covers/" + val.image + "')"
 			  });
 			  var label = $("<label/>", {
 			  	"text" : val.name
-			  })
+			  });
 			  image.appendTo(cover);
 			  label.appendTo(cover);
-			  cover.appendTo(link);
-			  link.appendTo(container);
+			  cover.appendTo(container);
+
+			  // Save
+			  var c = cover.get(0);
+			  c.key = key;
+			  c.image = val.image;
+			  c.name = val.name;
+			  _covers[key] = cover;
 		  });
+
+		  // Links
+		  for(var key in _covers)
+		  	_covers[key].click(function() { _select(this); })
 		 
 		 	// Filters
 	    tech.on('change', function(){
@@ -56,13 +76,14 @@ var covers = (function() {
 
 		 	// All done !
 		  container.mixItUp();
-		 	container.mixItUp('sort', "name:asc");
+		 	container.mixItUp('sort', "awesome:desc");
 		});
 
 	}
 
 	return {
-		init : _init
+		init : _init,
+		select : _select
 	}
 
 })();
